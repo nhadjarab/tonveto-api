f36451fbd8821432d60d53796f3d7470d13c13bf
Nassim HADJ-ARAB <nassim.hadjarab@gmail.com>
2022-08-18 12:24:29 +0300

    Setup prisma

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 import express, { Express, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
 import dotenv from "dotenv";
+
+const prisma = new PrismaClient();
 
 dotenv.config();
 
@@ -8,6 +12,13 @@ const port = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
+});
+
+app.post("/register", async (req: Request, res: Response) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(port || 3005, () => {
