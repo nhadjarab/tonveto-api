d25e9aa450ba8a23ed96c7cb6ad8e35728b3b9a5
Nassim HADJ-ARAB <nassim.hadjarab@gmail.com>
2022-09-20 01:40:49 +0300

    Deployment

--- prisma/migrations/20220821074032_added/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[owner_id]` on the table `Calendar` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Calendar_owner_id_key" ON "Calendar"("owner_id");
