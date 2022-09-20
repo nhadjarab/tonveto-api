2ecc653b20d25fa8d0c6f9fafa0cd4c29dcb1c1e
Nassim HADJ-ARAB <nassim.hadjarab@gmail.com>
2022-09-20 21:15:17 +0300

    Fix advanced search

--- controllers/search/search.ts
+++ controllers/search/search.ts
@@ -61,9 +61,15 @@ export const advancedSearch = async (
   prisma: PrismaClient
 ) => {
   try {
-    const { city , zip_code , clinic_name , vet_name , specialty  , address , country} = req.query;
-
-
+    const {
+      city,
+      zip_code,
+      clinic_name,
+      vet_name,
+      specialty,
+      address,
+      country,
+    } = req.query;
 
     const clinics = await prisma.clinic.findMany({
       where: {
@@ -80,25 +86,53 @@ export const advancedSearch = async (
       },
     });
 
+    // check if queries are undefined
+    if (
+      city == undefined &&
+      zip_code == undefined &&
+      clinic_name == undefined &&
+      vet_name == undefined &&
+      specialty == undefined &&
+      address == undefined &&
+      country == undefined
+    )
+      return res
+        .status(200)
+        .json({ filteredClinics: clinics, filteredVets: vets });
+
     // Filter clinics and vets by query
     const filteredClinics = clinics.filter(
       (clinic) =>
-        clinic.name.toLowerCase().includes((clinic_name as string)?.toLowerCase()) ||
+        clinic.name
+          .toLowerCase()
+          .includes((clinic_name as string)?.toLowerCase()) ||
         clinic.city.toLowerCase().includes((city as string)?.toLowerCase()) ||
-        clinic.address.toLowerCase().includes((address as string)?.toLowerCase()) ||
-        clinic.zip_code.toLowerCase().includes((zip_code as string)?.toLowerCase()) ||
-        clinic.country.toLowerCase().includes((country as string)?.toLowerCase())
+        clinic.address
+          .toLowerCase()
+          .includes((address as string)?.toLowerCase()) ||
+        clinic.zip_code
+          .toLowerCase()
+          .includes((zip_code as string)?.toLowerCase()) ||
+        clinic.country
+          .toLowerCase()
+          .includes((country as string)?.toLowerCase())
     );
 
     const filteredVets = vets.filter(
       (vet) =>
-        vet.first_name.toLowerCase().includes((vet_name as string)?.toLowerCase()) ||
-        vet.last_name.toLowerCase().includes((vet_name as string)?.toLowerCase()) ||
+        vet.first_name
+          .toLowerCase()
+          .includes((vet_name as string)?.toLowerCase()) ||
+        vet.last_name
+          .toLowerCase()
+          .includes((vet_name as string)?.toLowerCase()) ||
         (vet.first_name + " " + vet.last_name)
           .toLowerCase()
           .includes((vet_name as string)?.toLowerCase()) ||
         vet.specialities.some((speciality) =>
-          speciality.name.toLowerCase().includes((specialty as string)?.toLowerCase())
+          speciality.name
+            .toLowerCase()
+            .includes((specialty as string)?.toLowerCase())
         )
     );
 
